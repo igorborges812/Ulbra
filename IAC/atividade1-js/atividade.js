29480a8de705e5623b9344bd4fd5e25f32115027
igor borges <igorborges812@gmail.com>
2022-11-08 17:14:46 -0300

    atualização

---
+++ IAC/atividade1-js/atividade.js
@@ -0,0 +1,60 @@
+// 1)
+
+var a = 35;
+console.log (a, typeof a);
+
+// 5)
+
+var a = 3;
+var b = 3;
+var c = 3;
+
+var media = ((a + b + c) /3)
+    
+console.log(media)
+
+// 6)
+
+var ap1 = 1.5;
+var ap2 = 2.5;
+var As = 6;
+
+var mediaFinal = (ap1 + ap2 + As)
+
+
+console.log ("Sua média final é: ",mediaFinal)
+
+// 7)
+
+var name = "Igor";
+var age = 18;
+
+if (age >= 18) { 
+    console.log("Você é maior de idade, possui mais de 18 anos.")
+} else if (age <= 18) {
+    console.log ("Você não é maior de idade, possui menos de 18 anos.")
+}
+
+//8)
+
+var n1 = 40
+var n2 = 20
+var n3 = 30 
+
+if (n1>n2 & n1>n3) {
+    console.log(n1, "é o maior número")
+}
+else if (n2>n1 & n2>n3) {
+    console.log(n2, "é o maior número")
+}
+
+else if (n3>n1 & n3>n2) {
+    console.log(n3, "é o maior número")
+}
+
+
+
+
+
+
+
